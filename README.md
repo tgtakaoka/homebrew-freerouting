038062989f8442c463bbef0261cc487a87257685
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2020-04-03 16:07:44 +0900

    Update to mihosft.eu v1.4.2

--- README.md
+++ README.md
@@ -2,13 +2,16 @@ homebrew-freerouting
 ======================
 
 The [Homebrew][] formulae of [FreeRouting][], Java Based Printed
-Circuit Board Routing Software from FreeRouting.net written by Alfons
-Wirtz.
+Circuit Board Routing Software developped at [GitHub/freerouting] and
+maintained by [mihosoft.eu] (Originally written by [Alfons Wirtz][]).
 
 To get everything, execute the following commands.
 
     $ brew tap tgtakaoka/freerouting
-    $ brew install --HEAD freerouting
+    $ brew install freerouting
 
 [Homebrew]: https://brew.sh/
 [FreeRouting]: https://freerouting.org/
+[GitHub/freerouting]: https://github.com/freerouting/
+[mihosoft.eu]: https://freerouting.mihosoft.eu/
+[Alfons Wirtz]: http://freerouting.net/
--- freerouting.rb
+++ freerouting.rb
@@ -1,15 +1,16 @@
 class Freerouting < Formula
   desc "Printed Circuit Board Routing Software"
-  homepage "https://freerouting.org"
-  head "https://github.com/freerouting/freerouting.git"
+  homepage "https://freerouting.mihosoft.eu/"
+  url "https://bintray.com/miho/Freerouting/download_file?file_path=eu%2Fmihosoft%2Ffreerouting%2Ffreerouting%2F1.4.2%2Ffreerouting-1.4.2-executable.jar"
+  sha256 "e0235326c638d7869c88b974f94091f721ced092aa1f5da6fe219810a0f29c1d"
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.11+"
 
   def install
-    (libexec/"bin").install "binaries/FreeRouting.jar"
+    (libexec/"bin").install "freerouting-1.4.2-executable.jar"
     (bin/"FreeRouting").write <<~EOS
     #!/bin/bash
-    exec java -jar "#{libexec}/bin/FreeRouting.jar"
+    exec java -jar "#{libexec}/bin/freerouting-1.4.2-executable.jar"
     EOS
     chmod 0555, bin/"FreeRouting"
   end
